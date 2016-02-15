f222fa81f6eebbf3e730c44a73cafc46b88b2459
lesnikovskiy <lesnikovski@gmail.com>
2016-02-15 11:47:48 +0200

    props added

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 class Detail extends React.Component {
     render() {
-        return <p>This is React rendering HTML!!!</p>;
+        return <p>{this.props.message}</p>;
     }
 }
 
