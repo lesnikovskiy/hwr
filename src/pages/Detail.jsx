06e232e455e8fde8ff08005894cfba9ddbd6cb73
lesnikovskiy <lesnikovski@gmail.com>
2016-02-17 10:53:24 +0200

    implement ajax page with buttons

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -39,18 +39,67 @@ class Detail extends React.Component {
                 }
             });
     }
-       
-    render() {
-        return (
-            <div>
-                {this.state.commits.map((commit, index) => {
-                    const author = commit.author ? commit.author.login : "Anonymous";
-                    
-                    return (<p key={index}>
+    
+    renderCommits() {
+        return this.state.commits.map((commit, index) => {
+           const author = commit.author ? commit.author.login : "Anonymous";
+           
+           return (<p key={index}>
                         <strong>{author}</strong>:
                         <a href={commit.html_url}>{commit.commit.message}</a>
                     </p>);
-                })}
+        });
+    }
+    
+    renderForks() {
+        return this.state.forks.map((fork, index) => {
+           const owner = fork.owner ? fork.owner.login : "Anonymous";
+           
+           return (<p key={index}>
+                        <strong>{owner}</strong>:
+                        <a href={fork.html_url}>{fork.html_url}</a> at {fork.created_at}
+                    </p>);
+        });
+    }
+    
+    renderPulls() {
+        return this.state.pulls.map((pull, index) => {
+           const user = pull.user ? pull.user.login : "Anonymous";
+           
+           return (<p key={index}>
+                        <strong>{user}</strong>:
+                        <a href={pull.html_url}>{pull.body}</a>.
+                    </p>); 
+        });
+    }
+    
+    showCommits() {
+        this.setState({mode: "commits"});
+    }
+    showForks() {
+        this.setState({mode: "forks"});
+    }
+    showPulls() {
+        this.setState({mode: "pulls"})
+    }
+       
+    render() {
+        let content;
+        
+        if (this.state.mode === "commits") {
+            content = this.renderCommits();
+        } else if (this.state.mode === "forks") {
+            content = this.renderForks();
+        } else {
+            content = this.renderPulls();
+        }
+        
+        return (
+            <div>
+                <button onClick={this.showCommits.bind(this)}>Show Commits</button>
+                <button onClick={this.showForks.bind(this)}>Show Forks</button>
+                <button onClick={this.showPulls.bind(this)}>Show Pulls</button>
+                {content}
             </div>
         )
     }
